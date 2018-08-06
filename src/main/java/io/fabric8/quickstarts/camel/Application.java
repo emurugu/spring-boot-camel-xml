63b052aa9d1fc7ea4edd55701cf788397092b128
Elamurugu Kandasamy <elamurugu.kandasamy@spark.co.nz>
2018-08-06 14:28:44 +1200

    modified

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -16,11 +16,14 @@
  */
 package io.fabric8.quickstarts.camel;
 
+import org.apache.camel.opentracing.starter.CamelOpenTracing;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@CamelOpenTracing
 // load regular Spring XML file from the classpath that contains the Camel XML DSL
 @ImportResource({"classpath:spring/camel-context.xml"})
 public class Application {
